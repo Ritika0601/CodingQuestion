3fdec40faa83098a1925d279ccf0ed68556aaeca
Anish <anish@Swatis-Mac-mini.local>
2017-03-21 18:28:26 +0530

    i have changed ritikas file

--- Hello.c
+++ Hello.c
@@ -1,8 +1,8 @@
-// C problem printing hellow world
+   // C problem printing hellow world
 #include <stdio.h>
 #include <conio.h>
 void main()
 {
 	printf("Hellow world");
-	return 0;
+        	return 0;
 } 
